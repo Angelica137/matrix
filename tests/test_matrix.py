c82d6c586305741abce50fed9336577e0a374b35
Angelica <Angelica137@users.noreply.github.com>
2022-05-14 19:42:23 +0100

    add .rows(), red

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -4,3 +4,8 @@ from scripts.matrix import *
 def test_matris_returns_input_string():
     m = Matrix("1 2 3\n4 5 6\n7 8 9")
     assert m.numbers == "1 2 3\n4 5 6\n7 8 9"
+
+
+def test_matrix_returns_rows():
+    m = Matrix("1 2 3\n4 5 6\n7 8 9")
+    assert m.rows() == ["1 2 3", "4 5 6", "6 7 8"]
