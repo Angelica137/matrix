28e7eb27cb7b7d012925bac528fc7273f0652ce7
Angelica <Angelica137@users.noreply.github.com>
2022-05-19 18:30:36 +0100

    add same test for .columns(), red

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -26,11 +26,18 @@ def test_matrix_returns_columns():
     assert m.columns() == ["1 4 7", "2 5 8", "3 6 9"]
 
 
+def test_column_returns_column_2():
+    m = Matrix("89 1903 3\n18 3 1\n9 4 800")
+    assert m.columns() == ["89 1903 3", "18 3 1", "9 4 800"]
+
+
 def test_column_returns_single_column_1():
     m = Matrix("1")
     assert m.column(1) == [1]
 
 
+"""
 def test_column_returns_column_2():
     m = Matrix("89 1903 3\n18 3 1\n9 4 800")
     assert m.column(2) == [1903, 3, 4]
+"""
