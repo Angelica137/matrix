0b64e2a75ed784fb5b98f27cb9befccabf3eda53
Angelica <Angelica137@users.noreply.github.com>
2022-05-20 19:49:48 +0100

    fix typo in .columns() test 2

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -26,7 +26,7 @@ def test_matrix_returns_columns():
     assert m.columns() == [['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9']]
 
 
-def test_column_returns_columns_2():
+def test_columns_returns_columns_2():
     m = Matrix("89 1903 3\n18 3 1\n9 4 800")
     assert m.columns() == [['89', '18', '9'], [
         '1903', '3', '4'], ['3', '1', '800']]
