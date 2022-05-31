738b205bf32ebf09c882c0b359f594051f1a2430
Angelica <Angelica137@users.noreply.github.com>
2022-05-31 20:06:52 +0100

    refactor .row(), green

--- scripts/matrix.py
+++ scripts/matrix.py
@@ -15,8 +15,7 @@ class Matrix:
         Returns the row requested by row_number
         """
         single_row = self.matrix()[row_number - 1]
-        single_row = [int(single_row[i]) for i in range(len(single_row))]
-        return single_row
+        return [int(single_row[i]) for i in range(len(single_row))]
 
     def column(self, column_number) -> list:
         """
