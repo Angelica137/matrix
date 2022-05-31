27521657c96f7a831073c1ec1e65cd8a252d65c9
Angelica <Angelica137@users.noreply.github.com>
2022-05-31 20:05:55 +0100

    delete obsolete

--- scripts/matrix.py
+++ scripts/matrix.py
@@ -13,11 +13,6 @@ class Matrix:
     def row(self, row_number: int) -> list:
         """
         Returns the row requested by row_number
-
-        single_row = self.matrix()[row_number - 1]
-        for i in range(len(single_row)):
-            single_row[i] = int(single_row[i])
-        return single_row
         """
         single_row = self.matrix()[row_number - 1]
         single_row = [int(single_row[i]) for i in range(len(single_row))]
